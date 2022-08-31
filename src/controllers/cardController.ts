2a3882d874884014fbf604d5675fea766552fe06
Klaus Kupper <klausdk1999@gmail.com>
2022-08-31 20:21:07 -0300

    fim

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -1,10 +1,9 @@
 import { insert,CardInsertData } from "../repositories/cardRepository.js";
-import { cardService } from "../services/Services.js";
 
 export async function createCard(req, res) {
     const card:CardInsertData = req.body;
     try {
-     
+      
       await insert(card);
 
       return res.sendStatus(200);
--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
-
+const response={
+    winner: "fulano", // nulo se empate
+    loser: "ciclana", //nulo se empate
+	draw: false // true se empate
+};
 export async function battleService(req, res, next) {
     
     const promise = await axios.get(`http://api.github.com/users/${req.body.firstUser}/repos`);
@@ -11,7 +15,7 @@ export async function battleService(req, res, next) {
         atividade_petulante1+=promise.data[i].stargazers_count;
     }
 
-    const promise1 = await axios.get(`http://api.github.com/users/${req.body.secondUser}/repos`)
+    const promise1 = await axios.get(`http://api.github.com/users/${req.body.secondUser}/repos`);
 
     for(let i=0;i<promise1.data.length;i++){
         atividade_petulante2+=promise1.data[i].stargazers_count;
@@ -21,12 +25,25 @@ export async function battleService(req, res, next) {
     console.log(atividade_petulante2)
 
     if ( atividade_petulante1>atividade_petulante2){
+        res.status(200).send(response)
+        response.winner=req.body.firstUser;
+        response.loser=req.body.secondUser;
+        response.draw=false;
         console.log("1 é maior")
     }
     if ( atividade_petulante1===atividade_petulante2){
+        response.winner=null;
+        response.loser=null;
+        response.draw=true;
         console.log("empate")
     }else{
+        response.winner=req.body.secondUserfirstUser;
+        response.loser=req.body.firstUser;
+        response.draw=false;
         console.log("2 é maior")
     };
+
+    res.status(200).send(response);
+
     next();
 } 
